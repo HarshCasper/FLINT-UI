a9f6be7b0d565465ef4d237b6ff33fe89276be2e
Harsh Mishra <erbeusgriffincasper@gmail.com>
2022-04-12 18:46:12 +0530

    Update vue.config.js

--- flint.ui/vue.config.js
+++ flint.ui/vue.config.js
@@ -1,7 +1,8 @@
 // vue.config.js
 module.exports = {
   devServer: {
-    port: 8000
+    port: 8000,
+    proxy: 'https://nice-bush-04fb02600.1.azurestaticapps.net',
   },
   runtimeCompiler: true
 }
